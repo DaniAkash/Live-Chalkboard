229e5ac3c73a2228b623c71fa9b013db7b89168d
DaniAkash <eaglearrow.arrow@gmail.com>
2016-09-28 01:49:05 +0530

    port from env

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
+var port = process.env.PORT || 3000;
+http.listen(port, function(){
 	console.log('listening on *:3000');
 });
