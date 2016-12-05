427c65e00f9f54a651ade29924bac06cb2b665a9
Dani Akash <DaniAkash@users.noreply.github.com>
2016-12-05 21:49:48 +0530

    Update chalk.js

--- public/js/chalk.js
+++ public/js/chalk.js
@@ -3,7 +3,7 @@ $(document).ready(chalkboard);
 function chalkboard(){
 	$('#chalkboard').remove();
 	$('.chalk').remove();
-    $('body').prepend('<a href="https://github.com/DaniAkash/Live-Chalkboard"><img style="position: absolute; top: 0; right: 0; border: 0;height: 175px;width: 175px;z-index: 99999;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>');
+    $('body').prepend('<a href="https://github.com/DaniAkash/Live-Chalkboard/fork"><img style="position: absolute; top: 0; right: 0; border: 0;height: 175px;width: 175px;z-index: 99999;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>');
 	$('body').prepend('<div class="panel"><a class="link" target="_blank">Save</a></div>');
 	$('body').prepend('<img src="img/bg.png" id="pattern" width=50 height=50>');
 	$('body').prepend('<canvas id="chalkboard"></canvas>');
